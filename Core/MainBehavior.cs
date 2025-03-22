d00184f88f37e48f8945c76754c7036319d3c3a2
夏沫 <trenal@163.com>
2025-03-22 17:03:21 +0800

    Inited

---
+++ Core/MainBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using TextGameLib.Util;
+
+namespace TextGameLib.Core;
+
+/// <summary>
+/// 主要的游戏基类
+/// </summary>
+public abstract class MainBehavior
+{
+    /// <summary>
+    /// 输入玩家的操作字符串，返回对应的输出
+    /// </summary>
+    public abstract string InputAndOutput(string input);
+
+    /// <summary>
+    /// 关闭游戏后清理资源与内存等
+    /// </summary>
+    public abstract string Close();
+
+    #region DEBUG
+
+    [Conditional("DEBUG")]
+    public static void ValidateInheritance()
+    {
+        var allTypes = AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(asm => asm.GetTypes())
+            .Where(t => typeof(MainBehavior).IsAssignableFrom(t) && t != typeof(MainBehavior));
+
+        if (allTypes.Count() > 1)
+            throw new InvalidOperationException("只应该存在一个 MainBehavior 的继承类");
+
+        var attri = allTypes.First().GetCustomAttribute<MainBehaviorAttribute>() ?? throw new InvalidOperationException("MainBehavior 的继承类必须有 MainBehaviorAttribute 特性");
+    }
+
+    #endregion DEBUG
+}
---
+++ Util/MainBehaviorAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace TextGameLib.Util;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class MainBehaviorAttribute(string name, string version) : Attribute
+{
+    public string Name { get; internal set; } = name;
+
+    public string Version { get; internal set; } = version;
+}
