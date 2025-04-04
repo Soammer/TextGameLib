df9c02bc9f4573d78be5d1782f56eded2ab6f88f
夏沫 <trenal@163.com>
2025-04-04 19:36:22 +0800

    更新CSV读取，事件控制器

---
+++ Util/CsvReader.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace TextGameLib.Util;
+
+public class CsvReader(StreamReader reader)
+{
+    private StreamReader m_Reader = reader;
+    private string[] m_HeaderStrs;
+    public void ReadHeader()
+    {
+        // Read header from csv file
+        string? strs =  reader.ReadLine() ?? throw new Exception("Empty csv file");
+        m_HeaderStrs = strs.Split(',');
+    }
+
+    public List<T> WriteAllRecord<T>() where T : new()
+    {
+        // Write record to csv file
+        List<T> res = [];
+        Type t = typeof(T);
+        while (true)
+        {
+            T tmp = new();
+            string? strs = reader.ReadLine();
+            if(strs == null)
+                break;
+            string[] values = strs.Split(',');
+            string binaryString = "00000001";
+
+            // 将二进制字符串转换为int类型
+            int intValue = Convert.ToInt32(binaryString, 2);
+
+            for (int i = 0; i < m_HeaderStrs.Length; i++)
+            {
+                string str = m_HeaderStrs[i];
+                string input = values[i];
+                if(input.Length == 0)
+                    continue;
+                FieldInfo? fieldinfo = t.GetField(str) ?? throw new Exception("Field not found");
+                Type ot = fieldinfo.FieldType;
+                fieldinfo.SetValue(tmp, Convert.ChangeType(input, ot));
+            }
+            res.Add(tmp);
+        }
+        return res;
+    }
+}
--- Util/EventDispatcher.cs
+++ Util/EventDispatcher.cs
@@ -1,136 +1,196 @@
-﻿using TextGameLib.Interfaces;
+﻿using System.Diagnostics;
+using System.Reflection;
+using TextGameLib.Interfaces;
 
 namespace TextGameLib.Util;
 
 /// <summary>
-/// 局部事件分发器
+/// 事件管理类
 /// </summary>
-public class EventDispatcher : IMemory
+public static class EventDispatcher
 {
-    private readonly Dictionary<int, List<EventRegInfo>> _allEventListenerDic;
-
-    public EventDispatcher()
-    {
-        _allEventListenerDic = [];
-    }
-
-    public void AddEventListener(int eventId, Action eventCallback)
-    {
-        AddEventListenerImp(eventId, eventCallback);
-    }
-
-    public void AddEventListener<T>(int eventId, Action<T> eventCallback)
-    {
-        AddEventListenerImp(eventId, eventCallback);
-    }
-
-    public void AddEventListener<T1, T2>(int eventId, Action<T1, T2> eventCallback)
-    {
-        AddEventListenerImp(eventId, eventCallback);
-    }
-
-    public void AddEventListener<T1, T2, T3>(int eventId, Action<T1, T2, T3> eventCallback)
-    {
-        AddEventListenerImp(eventId, eventCallback);
-    }
+    private static readonly Dictionary<int, List<Delegate>> m_EventTable = new();
 
     /// <summary>
-    /// 移除所有事件监听。
+    /// 添加无参数事件
     /// </summary>
-    public void DestroyAllEventListener()
+    /// <param name="eventType">事件的ID</param>
+    /// <param name="callBack">事件</param>
+    public static void RegisterEvent(int eventType, Action callBack)
     {
-        foreach (var kv in _allEventListenerDic)
+        if (OnListenerAdding(eventType, callBack))
         {
-            foreach (var eventRegInfo in kv.Value)
-            {
-                EventRegInfo.Release(eventRegInfo);
-            }
-            kv.Value.Clear();
+            if (!m_EventTable[eventType].Contains(callBack))
+                m_EventTable[eventType].Add(callBack);
         }
-        _allEventListenerDic.Clear();
     }
 
     /// <summary>
-    /// 删除监听
+    /// 添加单参数事件
     /// </summary>
-    public void RemoveEventListener(int eventID, Delegate listener)
+    public static void RegisterEvent<T>(int eventType, Action<T> callBack)
     {
-        if (_allEventListenerDic.TryGetValue(eventID, out var listenerList))
+        if (OnListenerAdding(eventType, callBack))
         {
-            foreach (var node in listenerList)
+            if (!m_EventTable[eventType].Contains(callBack))
+                m_EventTable[eventType].Add(callBack);
+        }
+    }
+
+    /// <summary>
+    /// 添加双参数事件
+    /// </summary>
+    public static void RegisterEvent<T1, T2>(int eventType, Action<T1, T2> callBack)
+    {
+        if (OnListenerAdding(eventType, callBack))
+        {
+            if (!m_EventTable[eventType].Contains(callBack))
+                m_EventTable[eventType].Add(callBack);
+        }
+    }
+
+    /// <summary>
+    /// 添加三参数事件
+    /// </summa
+    public static void RegisterEvent<T1, T2, T3>(int eventType, Action<T1, T2, T3> callBack)
+    {
+        if (OnListenerAdding(eventType, callBack))
+        {
+            if (!m_EventTable[eventType].Contains(callBack))
+                m_EventTable[eventType].Add(callBack);
+        }
+    }
+
+    /// <summary>
+    /// 移除无参数事件
+    /// </summary>
+    public static void RemoveEvent(int eventType, Action callBack)
+    {
+        m_EventTable[eventType].Remove(callBack);
+
+        // 事件的挂载频繁，不做移除处理
+        //OnListenerRemoved(eventType);
+    }
+
+    /// <summary>
+    /// 移除单参数事件
+    /// </summary>
+    public static void RemoveEvent<T>(int eventType, Action<T> callBack)
+    {
+        m_EventTable[eventType].Remove(callBack);
+    }
+
+    /// <summary>
+    /// 移除双参数事件
+    /// </summary>
+    public static void RemoveEvent<T1, T2>(int eventType, Action<T1, T2> callBack)
+    {
+        m_EventTable[eventType].Remove(callBack);
+    }
+
+    /// <summary>
+    /// 移除三参数事件
+    /// </summary>
+    public static void RemoveEvent<T1, T2, T3>(int eventType, Action<T1, T2, T3> callBack)
+    {
+        m_EventTable[eventType].Remove(callBack);
+    }
+
+    /// <summary>
+    /// 对某个事件进行完全移除
+    /// </summary>
+    /// <param name="eventType"></param>
+    public static void RemoveAll(int eventType)
+    {
+        if (m_EventTable.ContainsKey(eventType))
+            m_EventTable[eventType] = null;
+    }
+
+    /// <summary>
+    /// 调用无参数事件，倒序比较安全
+    /// </summary>
+    public static void FireEvent(int eventType)
+    {
+        if (m_EventTable.TryGetValue(eventType, out var list))
+        {
+            var cache = list.ToArray(); // TODO 优化，List._version不给访问
+            for (int i = cache.Length - 1; i >= 0; i--)
             {
-                if (node.Callback == listener)
+                if (cache[i] is Action action)
                 {
-                    node.IsDeleted = true;
-                    break;
+                    action();
                 }
             }
         }
     }
 
     /// <summary>
-    /// 增加事件监听具体实现。
+    /// 调用单参数事件
     /// </summary>
-    private void AddEventListenerImp(int eventId, Delegate listener)
+    public static void FireEvent<T>(int eventType, T args)
     {
-        if (!_allEventListenerDic.TryGetValue(eventId, out var listenerList))
+        if (m_EventTable.TryGetValue(eventType, out var list))
         {
-            listenerList = [];
-            _allEventListenerDic.Add(eventId, listenerList);
+            var cache = list.ToArray();
+            for (int i = cache.Length - 1; i >= 0; i--)
+            {
+                if (cache[i] is Action<T> actionT)
+                {
+                    actionT(args);
+                }
+            }
         }
-
-        var existNode = listenerList.Find(node => node.Callback == listener);
-        if (existNode != null)
-        {
-            existNode.IsDeleted = false;
-            return;
-        }
-
-        listenerList.Add(EventRegInfo.Alloc(listener));
     }
 
-    public void Clear()
+    /// <summary>
+    /// 调用双参数事件
+    /// </summary>
+    public static void FireEvent<T1, T2>(int eventType, T1 arg1, T2 arg2)
     {
-        DestroyAllEventListener();
+        if (m_EventTable.TryGetValue(eventType, out var list))
+        {
+            var cache = list.ToArray();
+            for (int i = cache.Length - 1; i >= 0; i--)
+            {
+                if (cache[i] is Action<T1, T2> actionTT)
+                {
+                    actionTT(arg1, arg2);
+                }
+            }
+        }
     }
 
-    public class EventRegInfo : IMemory
+    /// <summary>
+    /// 调用三参数事件
+    /// </summary>
+    public static void FireEvent<T1, T2, T3>(int eventType, T1 arg1, T2 arg2, T3 arg3)
     {
-        /// <summary>
-        /// 事件回调
-        /// </summary>
-        public Delegate? Callback;
-
-        /// <summary>
-        /// 事件是否删除
-        /// </summary>
-        public bool IsDeleted;
-
-        public EventRegInfo(Delegate callback)
+        if (m_EventTable.TryGetValue(eventType, out var list))
         {
-            Callback = callback;
-            IsDeleted = false;
+            var cache = list.ToArray();
+            for (int i = cache.Length - 1; i >= 0; i--)
+            {
+                if (cache[i] is Action<T1, T2, T3> actionTTT)
+                {
+                    actionTTT(arg1, arg2, arg3);
+                }
+            }
         }
+    }
 
-        public EventRegInfo()
-        { }
-
-        public void Clear()
+    /// <summary>
+    /// 尝试添加事件监听的时候调用，防止出现异常，当然，只有编辑器时期会执行
+    /// </summary>
+    /// <returns>是否满足添加事件监听的条件</returns>
+    private static bool OnListenerAdding(int eventType, Delegate handler)
+    {
+        if (!m_EventTable.ContainsKey(eventType))
         {
-            IsDeleted = false;
+            m_EventTable.Add(eventType, []);
         }
-
-        public static EventRegInfo Alloc(Delegate callback)
-        {
-            EventRegInfo ret = MemoryPool.Acquire<EventRegInfo>();
-            ret.Callback = callback;
-            ret.IsDeleted = false;
-            return ret;
-        }
-
-        public static void Release(EventRegInfo eventRegInfo)
-        {
-            MemoryPool.Release(eventRegInfo);
-        }
+        List<Delegate> list = m_EventTable[eventType];
+        if (list == null)
+            return false;
+        return true;
     }
 }
